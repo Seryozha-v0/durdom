6210d4d3d9039df1c10124f59b87e2418110bac7
Seryozha <syt.an@ya.ru>
2023-08-08 11:24:39 +0300

    real time searching

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -3,19 +3,38 @@ import SearchIcon from './../../img/icons/search.svg';
 
 import './search.scss';
 
-const Search = ({ icon, short }) => {
+const Search = ({ icon, short, result, value, onChange, onFocus, onBlur }) => {
     return (
-        <form class={clsx("search", {"search_short": short})}>
-            {icon && (
-                <div class="search__icon">
-                    <img src={icon} alt="calendar" />
+        <div class={clsx("search", { "search_short": short })}>
+            <form class={clsx("search__form", { "search__form_active": result?.length })}>
+                {icon && (
+                    <div class="search__icon">
+                        <img src={icon} alt="calendar" />
+                    </div>
+                )}
+                <input
+                    class="search__input"
+                    placeholder="Поиск среди 3 274 проектов"
+                    type="text"
+                    value={value}
+                    onChange={onChange}
+                    onFocus={onFocus}
+                    onBlur={onBlur}
+                />
+                <button class="search__btn" type="submit">
+                    <img src={SearchIcon} alt="search" />
+                </button>
+            </form>
+            {!!result?.length && (
+                <div class="search__result">
+                    {result.map((item, i) => (
+                        <div key={i} className="search__resultItem">
+                            {item.name} - {item.date}
+                        </div>
+                    ))}
                 </div>
             )}
-            <input class="search__input" placeholder="Поиск среди 3 274 проектов" type="text" />
-            <button class="search__btn" type="submit">
-                <img src={SearchIcon} alt="search" />
-            </button>
-        </form>
+        </div>
     )
 }
 
--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import Button from '../../components/button/Button';
 import CardsList from '../../components/cardsList/CardsList';
 import Search from '../../components/search/Search';
@@ -7,8 +8,47 @@ import { ReactComponent as FindTeamIcon } from './../../img/icons/job.svg';
 import art from "./../../img/pictures/Art-2.png";
 
 import './main.scss';
+import { test } from '../../data/adminData';
 
 const Main = () => {
+    const [searchText, setSearchText] = useState('');
+    const [searchFindable, setSearchFindable] = useState([]);
+    const [searchFocus, setSearchFocus] = useState(false);
+
+    const handleSearchText = (e) => {
+        const text = e.target.value;
+        setSearchText(text);
+    }
+
+    const getProjects = () => {
+        const projects = test;
+        const lowerSearchText = searchText.toLowerCase();
+
+        const result = projects.filter((item) => {
+            const name = item.name.toLowerCase();
+            const textProject = item.text.toLowerCase();
+            return name.includes(lowerSearchText) || textProject.includes(lowerSearchText);
+        });
+
+        setSearchFindable(result);
+    }
+    
+    const handleInputFocus = (e) => {
+        const type = e.type;
+        if (type === 'focus') {
+            if (searchText) getProjects();
+            return setSearchFocus(true)
+        };
+        setSearchFocus(false);
+        setSearchFindable([]);
+    }
+
+    useEffect(() => {
+        if (searchFocus) {
+            getProjects();
+        }
+    }, [searchText]);
+
     return (
         <>
             <section class='present'>
@@ -37,14 +77,19 @@ const Main = () => {
             </section>
             <section class="sec-mainSearch">
                 <div class="container">
-                    <Search 
+                    <Search
                         short={true}
+                        value={searchText}
+                        onChange={handleSearchText}
+                        onFocus={handleInputFocus}
+                        onBlur={handleInputFocus}
+                        result={searchFindable}
                     />
                 </div>
             </section>
             <section class="sec-mainProjects">
                 <div class="container">
-                    <CardsList 
+                    <CardsList
                         title='Новые проекты'
                     />
                 </div>
